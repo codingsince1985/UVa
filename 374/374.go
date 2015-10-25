9af3e3cbacbdc6bd7106a351fee64a7e5ff18c43
Jerry Zhao <jchao@y7mail.com>
2015-10-25 15:30:22 +1100

    UVa 374 - Big Mod

---
+++ 374/374.go
@@ -0,0 +1,35 @@
+// UVa 374 - Big Mod
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func mod(b, p, m int) int {
+	if p == 0 {
+		return 1;
+	}
+	if p % 2 == 0 {
+		tmp := mod(b, p / 2, m)
+		return tmp * tmp % m
+	} else {
+		return (b % m) * mod(b, p - 1, m) % m
+	}
+}
+
+
+func main() {
+	in, _ := os.Open("374.in")
+	out, _ := os.Create("374.out")
+
+	var b, p, m int
+	for {
+		_, err := fmt.Fscanf(in, "%d\n%d\n%d\n\n", &b, &p, &m)
+		if (err != nil) {
+			break;
+		}
+		fmt.Fprintln(out, mod(b, p, m))
+	}
+}
