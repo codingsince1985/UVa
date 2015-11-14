5e977ceed408a142cbe2901d2ce89669df2fd4ec
Jerry Zhao <jchao@y7mail.com>
2015-11-14 12:30:25 +1100

    UVa 10303 - How Many Trees?

---
+++ 10303/10303.go
@@ -0,0 +1,46 @@
+// UVa 10303 - How Many Trees?
+
+// f[n] = C(2*n,n)/(n+1)
+// f[n] = f[0]*f[n-1]+f[1]*f[n-2]+...+f[n-1]*f[0]
+// f[n] = (4n-2)*f[n-1]/(n+1)
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"math/big"
+)
+
+const MAX = 1001
+
+var t [MAX]big.Int
+
+func prepare() {
+	t[1].SetInt64(1)
+	var tmp big.Int
+	for i := 2; i < MAX; i++ {
+		tmp.SetInt64(int64(i + 1))
+		t[i].SetInt64(int64(4 * i - 2))
+		t[i].Mul(&t[i], &t[i - 1])
+		t[i].Div(&t[i], &tmp)
+	}
+}
+
+func main() {
+	prepare()
+
+	in, _ := os.Open("10303.in")
+	out, _ := os.Create("10303.out")
+	var n int
+	for {
+		_, err := fmt.Fscanf(in, "%d", &n)
+		if err != nil {
+			break
+		}
+		fmt.Fprintln(out, &t[n])
+	}
+
+	in.Close()
+	out.Close()
+}
