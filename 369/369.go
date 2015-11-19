9f1eef2ede9e8493c5ce81a69e452c97ae34bc00
Jerry Zhao <jchao@y7mail.com>
2015-11-19 17:05:18 +1100

    UVa 369 - Combinations

---
+++ 369/369.go
@@ -0,0 +1,44 @@
+// UVa 369 - Combinations
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"math/big"
+)
+
+func f(n int) big.Int {
+	var r, t big.Int
+	r.SetInt64(1)
+	for i := 2; i <= n; i++ {
+		t.SetInt64(int64(i))
+		r.Mul(&r, &t)
+	}
+	return r
+}
+
+func c(n, m int) big.Int {
+	r := f(n)
+	s := f(n - m)
+	t := f(m)
+	r.Div(&r, &s)
+	r.Div(&r, &t)
+	return r
+}
+
+func main() {
+	in, _ := os.Open("369.in")
+	out, _ := os.Create("369.out")
+	var n, m int
+	for {
+		fmt.Fscanf(in, "%d %d", &n, &m)
+		if (n == 0 && m == 0) {
+			break
+		}
+		r := c(n, m)
+		fmt.Fprintf(out, "%d things taken %d at a time is %v exactly.\n", n, m, &r)
+	}
+	in.Close()
+	out.Close()
+}
