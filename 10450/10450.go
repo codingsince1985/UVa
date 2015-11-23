41618f04c846d15062d9d289b4daed24843b51d9
Jerry Zhao <jchao@y7mail.com>
2015-11-23 17:16:58 +1100

    UVa 10450 - World Cup Noise

---
+++ 10450/10450.go
@@ -0,0 +1,42 @@
+// UVa 10450 - World Cup Noise
+
+// f(n) = f(n-2) + f(n-1)
+// f(n-2) is the number of sequences starting with 10, followed by n-2 bit
+// f(n-1) is the number of sequences starting with 0, followed by n-1 bit
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var c [51]uint64
+
+func prepare() {
+	c[0] = 1
+	c[1] = 2
+}
+
+func f(b int) uint64 {
+	if c[b] != 0 {
+		return c[b]
+	}
+	c[b] = f(b - 1) + f(b - 2)
+	return c[b]
+}
+
+func main() {
+	prepare()
+	in, _ := os.Open("10450.in")
+	out, _ := os.Create("10450.out")
+	var n int
+	fmt.Fscanf(in, "%d", &n)
+	var b int
+	for i := 0; i < n; i++ {
+		fmt.Fscanf(in, "%d", &b)
+		fmt.Fprintf(out, "Scenario #%d:\n%v\n\n", i + 1, f(b))
+	}
+	in.Close()
+	out.Close()
+}
