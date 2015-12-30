f63736314e51ef4c4b1699b827ee4387d2ad684c
Jerry Zhao <jchao@y7mail.com>
2015-12-30 11:06:41 +1100

    UVa 357 - Let Me Count The Ways

---
+++ 357/357.go
@@ -0,0 +1,37 @@
+// UVa 357 - Let Me Count The Ways
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var cs = []int{1, 5, 10, 25, 50}
+
+func main() {
+	in, _ := os.Open("357.in")
+	defer in.Close()
+	out, _ := os.Create("357.out")
+	defer out.Close()
+
+	var v int
+	var dp []int64
+	l := len(cs)
+	for {
+		if _, err := fmt.Fscanf(in, "%d", &v); err != nil {
+			break
+		}
+
+		dp = make([]int64, v + 1)
+		dp[0] = 1
+
+		for i := 0; i < l; i++ {
+			c := cs[i]
+			for j := c; j <= v; j++ {
+				dp[j] += dp[j - c]
+			}
+		}
+		fmt.Fprintf(out, "There are %v ways to produce %d cents change.\n", dp[v], v)
+	}
+}
