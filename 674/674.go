d352e3d00eb87c7d519eaad81893716f80298a5b
Jerry Zhao <jchao@y7mail.com>
2015-12-31 12:27:41 +1100

    UVa 674 - Coin Change

---
+++ 674/674.go
@@ -0,0 +1,36 @@
+// UVa 674 - Coin Change
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var cs = []int{1, 5, 10, 25, 50}
+var l = len(cs)
+
+func main() {
+	in, _ := os.Open("674.in")
+	defer in.Close()
+	out, _ := os.Create("674.out")
+	defer out.Close()
+
+	var v int
+	var dp []uint64
+	for {
+		if _, err := fmt.Fscanf(in, "%d", &v); err != nil {
+			break
+		}
+		dp = make([]uint64, v + 1)
+		dp[0] = 1
+
+		for i := 0; i < l; i++ {
+			c := cs[i]
+			for j := c; j <= v; j++ {
+				dp[j] += dp[j - c]
+			}
+		}
+		fmt.Fprintln(out, dp[v])
+	}
+}
