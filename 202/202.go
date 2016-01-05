07137e9646fd2fde15147fa50af30de4a078d268
Jerry Zhao <jchao@y7mail.com>
2016-01-05 11:08:20 +1100

    UVa 202 - Repeating Decimals

---
+++ 202/202.go
@@ -0,0 +1,62 @@
+// UVa 202 - Repeating Decimals
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var m map[int]int
+
+func div(n, d int) (string, int) {
+	var ans string
+	m = make(map[int]int)
+	var w, r, count, idx int
+	var ok bool
+	w = n / d
+	ans += strconv.Itoa(w) + "."
+	r = n % d
+	m[r] = count
+	for {
+		count ++
+		r *= 10
+		if r < d {
+			ans += "0"
+			if idx, ok = m[r]; ok {
+				break
+			}
+		} else {
+			ans += strconv.Itoa(r / d)
+			r = r % d
+			if idx, ok = m[r]; ok {
+				break
+			}
+			m[r] = count
+		}
+	}
+	return ans, count - idx
+}
+
+func output(out *os.File, n, d int, ans string, rep int) {
+	fmt.Fprintf(out, "%d/%d = ", n, d)
+	fmt.Fprint(out, ans[:len(ans) - rep], "(", ans[len(ans) - rep:], ")")
+	fmt.Fprintf(out, "\n   %d = number of digits in repeating cycle\n\n", rep)
+}
+
+func main() {
+	in, _ := os.Open("202.in")
+	defer in.Close()
+	out, _ := os.Create("202.out")
+	defer out.Close()
+
+	var n, d int
+	for {
+		if _, err := fmt.Fscanf(in, "%d%d", &n, &d); err != nil {
+			break
+		}
+		ans, rep := div(n, d)
+		output(out, n, d, ans, rep)
+	}
+}
