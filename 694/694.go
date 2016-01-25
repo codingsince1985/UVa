e7299424370ea0c390949c186282b8fd140246d5
Jerry Zhao <jchao@y7mail.com>
2016-01-25 17:14:20 +1100

    UVa 694 - The Collatz Sequence

---
+++ 694/694.go
@@ -0,0 +1,44 @@
+// UVa 694 - The Collatz Sequence
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func solve(a, l int) int {
+	count := 0
+	for {
+		if a > l {
+			break;
+		}
+		count ++
+		if a == 1 {
+			break
+		}
+		if a % 2 == 0 {
+			a /= 2
+		} else {
+			a = 3 * a + 1
+		}
+	}
+	return count
+}
+
+func main() {
+	in, _ := os.Open("694.in")
+	defer in.Close()
+	out, _ := os.Create("694.out")
+	defer out.Close()
+
+	var a, l, kase int
+	for {
+		fmt.Fscanf(in, "%d%d", &a, &l)
+		if a < 0 {
+			break
+		}
+		kase ++
+		fmt.Fprintf(out, "Case %d: A = %d, limit = %d, number of terms = %d\n", kase, a, l, solve(a, l))
+	}
+}
