a5e0eae08fa6c0ab8e50187534edfbd7ee4d77cd
Jerry Zhao <jchao@y7mail.com>
2016-04-16 10:06:26 +1000

    UVa 489 - Hangman Judge

---
+++ 489/489.go
@@ -0,0 +1,49 @@
+// UVa 489 - Hangman Judge
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func solve(out *os.File, solution, guess string) {
+	chars := make(map[byte]bool)
+	for i := range solution {
+		chars[solution[i]] = true
+	}
+	count := 0
+	for i := range guess {
+		if chars[guess[i]] {
+			delete(chars, guess[i])
+			if len(chars) == 0 {
+				fmt.Fprintln(out, "You win.")
+				return
+			}
+		} else {
+			if count++; count == 7 {
+				fmt.Fprintln(out, "You lose.")
+				return
+			}
+		}
+	}
+	fmt.Fprintln(out, "You chickened out.")
+}
+
+func main() {
+	in, _ := os.Open("489.in")
+	defer in.Close()
+	out, _ := os.Create("489.out")
+	defer out.Close()
+
+	var kase int
+	var solution, guess string
+	for {
+		if fmt.Fscanf(in, "%d", &kase); kase == -1 {
+			break
+		}
+		fmt.Fprintf(out, "Round %d\n", kase)
+		fmt.Fscanf(in, "%s\n%s", &solution, &guess)
+		solve(out, solution, guess)
+	}
+}
