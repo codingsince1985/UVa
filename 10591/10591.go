5abe01d12f11397cc27b7c2e411778ba14263b3b
Jerry Zhao <jchao@y7mail.com>
2016-06-10 10:05:28 +1000

    UVa 10591 - Happy Number

---
+++ 10591/10591.go
@@ -0,0 +1,47 @@
+// UVa 10591 - Happy Number
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func happy(num int) bool {
+	visited := make(map[int]bool)
+	visited[num] = true
+	for {
+		sum := 0
+		for num > 0 {
+			sum += (num % 10) * (num % 10)
+			num /= 10
+		}
+		switch {
+		case sum == 1:
+			return true
+		case visited[sum]:
+			return false
+		}
+		visited[sum] = true
+		num = sum
+	}
+}
+
+func main() {
+	in, _ := os.Open("10591.in")
+	defer in.Close()
+	out, _ := os.Create("10591.out")
+	defer out.Close()
+
+	var n, num int
+	fmt.Fscanf(in, "%d", &n)
+	for i := 1; i <= n; i++ {
+		fmt.Fscanf(in, "%d", &num)
+		fmt.Fprintf(out, "Case #%d: %d is ", i, num)
+		if happy(num) {
+			fmt.Fprintln(out, "a Happy number.")
+		} else {
+			fmt.Fprintln(out, "an Unhappy number.")
+		}
+	}
+}
--- README.md
+++ README.md
@@ -222,6 +222,7 @@ UVa 10515 - Powers Et Al.
 UVa 10519 - !! Really Strange !!
 UVa 10530 - Guessing Game
 UVa 10579 - Fibonacci Numbers
+UVa 10591 - Happy Number
 UVa 10696 - f91
 UVa 10699 - Count the factors
 UVa 10783 - Odd Sum
