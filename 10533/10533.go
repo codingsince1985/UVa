6fe379dd8e91062125f706b0ec9b6fd8375722c6
Jerry Zhao <jchao@y7mail.com>
2016-07-21 17:18:37 +1000

    UVa 10533 - Digit Primes

---
+++ 10533/10533.go
@@ -0,0 +1,54 @@
+// UVa 10533 - Digit Primes
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const MAX = 1000000
+
+func sieve() []bool {
+	p := make([]bool, MAX+1)
+	p[0], p[1] = true, true
+	for i := 2; i*i <= MAX; i++ {
+		if !p[i] {
+			for j := i + i; j <= MAX; j += i {
+				p[j] = true
+			}
+		}
+	}
+	return p
+}
+
+func digitSum(n int) int {
+	total := 0
+	for n > 0 {
+		total += n % 10
+		n /= 10
+	}
+	return total
+}
+
+func main() {
+	in, _ := os.Open("10533.in")
+	defer in.Close()
+	out, _ := os.Create("10533.out")
+	defer out.Close()
+
+	p := sieve()
+	var n, t1, t2 int
+	fmt.Fscanf(in, "%d", &n)
+	for n > 0 {
+		n--
+		fmt.Fscanf(in, "%d%d", &t1, &t2)
+		cnt := 0
+		for i := t1; i <= t2; i++ {
+			if !p[i] && !p[digitSum(i)] {
+				cnt++
+			}
+		}
+		fmt.Fprintln(out, cnt)
+	}
+}
