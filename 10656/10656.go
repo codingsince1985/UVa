8e3a4b69151982b537bf521fb9bbd1e1bb845e50
Jerry Zhao <jchao@y7mail.com>
2016-11-18 00:01:24 +1100

    UVa 10656 - Maximum Sum (II)

---
+++ 10656/10656.go
@@ -0,0 +1,36 @@
+// UVa 10656 - Maximum Sum (II)
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	in, _ := os.Open("10656.in")
+	defer in.Close()
+	out, _ := os.Create("10656.out")
+	defer out.Close()
+
+	var n, tmp int
+	for {
+		if fmt.Fscanf(in, "%d", &n); n == 0 {
+			break
+		}
+		var sub []string
+		for i := 0; i < n; i++ {
+			fmt.Fscanf(in, "%d", &tmp)
+			if tmp > 0 {
+				sub = append(sub, strconv.Itoa(tmp))
+			}
+		}
+		if len(sub) == 0 {
+			fmt.Fprintln(out, 0)
+		} else {
+			fmt.Fprintln(out, strings.Join(sub, " "))
+		}
+	}
+}
