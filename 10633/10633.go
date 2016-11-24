b9ba51b2ee8a7dfa119ba605ef29a96a7ab27843
Jerry Zhao <jchao@y7mail.com>
2016-11-25 00:01:03 +1100

    UVa 10633 - Rare Easy Problem

---
+++ 10633/10633.go
@@ -0,0 +1,28 @@
+// UVa 10633 - Rare Easy Problem
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	in, _ := os.Open("10633.in")
+	defer in.Close()
+	out, _ := os.Create("10633.out")
+	defer out.Close()
+
+	var nm uint64
+	for {
+		if fmt.Fscanf(in, "%d", &nm); nm == 0 {
+			break
+		}
+		n := (nm * 10) / 9
+		if nm%9 == 0 {
+			fmt.Fprintln(out, n-1, n)
+		} else {
+			fmt.Fprintln(out, n)
+		}
+	}
+}
