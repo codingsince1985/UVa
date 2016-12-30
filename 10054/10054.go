d775cd591d03af684ecea44d3c51760a25197328
Jerry Zhao <jchao@y7mail.com>
2016-12-31 00:34:00 +1100

    UVa 10054 - The Necklace

---
+++ 10054/10054.go
@@ -0,0 +1,70 @@
+// UVa 10054 - The Necklace
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	out    *os.File
+	matrix [51][51]int
+	degree [51]int
+)
+
+func initialise() {
+	for i := range matrix {
+		for j := range matrix[i] {
+			matrix[i][j] = 0
+		}
+	}
+	for i := range degree {
+		degree[i] = 0
+	}
+}
+
+func euler(v int) {
+	for i := range matrix {
+		if matrix[v][i] != 0 {
+			matrix[v][i]--
+			matrix[i][v]--
+			euler(i)
+			fmt.Fprintln(out, i, v)
+		}
+	}
+}
+
+func solve(v int) {
+	for _, vi := range degree {
+		if vi%2 != 0 {
+			fmt.Fprintln(out, "some beads may be lost")
+			return
+		}
+	}
+	euler(v)
+}
+
+func main() {
+	in, _ := os.Open("10054.in")
+	defer in.Close()
+	out, _ = os.Create("10054.out")
+	defer out.Close()
+
+	var t, n, l, r, kase int
+	for fmt.Fscanf(in, "%d", &t); t > 0; t, kase = t-1, kase+1 {
+		initialise()
+		for fmt.Fscanf(in, "%d", &n); n > 0; n-- {
+			fmt.Fscanf(in, "%d%d", &l, &r)
+			matrix[l][r]++
+			matrix[r][l]++
+			degree[l]++
+			degree[r]++
+		}
+		fmt.Fprintf(out, "Case #%d\n", kase+1)
+		solve(r)
+		if t > 1 {
+			fmt.Fprintln(out)
+		}
+	}
+}
