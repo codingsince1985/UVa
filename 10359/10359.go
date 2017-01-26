2212c72f9e008cd122d82d55b9aac8886971fadc
Jerry Zhao <jchao@y7mail.com>
2017-01-27 00:04:47 +1100

    UVa 10359 - Tiling

---
+++ 10359/10359.go
@@ -0,0 +1,39 @@
+// UVa 10359 - Tiling
+
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"os"
+)
+
+const MAX = 250
+
+var cache = func() []big.Int {
+	dp := make([]big.Int, MAX+1)
+	dp[0].SetInt64(1)
+	dp[1].SetInt64(1)
+	for i := 2; i <= MAX; i++ {
+		tmp := big.NewInt(2)
+		tmp.Mul(tmp, &dp[i-2])
+		tmp.Add(tmp, &dp[i-1])
+		dp[i] = *tmp
+	}
+	return dp
+}()
+
+func main() {
+	in, _ := os.Open("10359.in")
+	defer in.Close()
+	out, _ := os.Create("10359.out")
+	defer out.Close()
+
+	var n int
+	for {
+		if _, err := fmt.Fscanf(in, "%d", &n); err != nil {
+			break
+		}
+		fmt.Fprintln(out, &cache[n])
+	}
+}
