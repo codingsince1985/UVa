ad9f384dc3019b871dbdffa6df6ca115ccc93d00
Jerry Zhao <jchao@y7mail.com>
2017-02-20 00:00:18 +1100

    UVa 10363 - Tic Tac Toe

---
+++ 10363/10363.go
@@ -0,0 +1,78 @@
+// UVa 10363 - Tic Tac Toe
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var grid [][]byte
+
+func win(player byte) bool {
+	for _, row := range grid {
+		if row[0] == row[1] && row[1] == row[2] && row[2] == player {
+			return true
+		}
+	}
+	for i := 0; i < 3; i++ {
+		if grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i] && grid[2][i] == player {
+			return true
+		}
+	}
+	return grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] && grid[2][2] == player ||
+		grid[2][0] == grid[1][1] && grid[1][1] == grid[0][2] && grid[0][2] == player
+}
+
+func count(player byte) int {
+	var num int
+	for _, row := range grid {
+		for _, cell := range row {
+			if cell == player {
+				num++
+			}
+		}
+	}
+	return num
+}
+
+func solve() bool {
+	xs, os := count('X'), count('O')
+	xwin, owin := win('X'), win('O')
+	switch {
+	case !xwin && !owin:
+		return xs == os || xs-1 == os
+	case xwin && !owin:
+		return xs-1 == os
+	case owin && !xwin:
+		return xs == os
+	default:
+		return false
+	}
+}
+
+func main() {
+	in, _ := os.Open("10363.in")
+	defer in.Close()
+	out, _ := os.Create("10363.out")
+	defer out.Close()
+
+	var n int
+	var line string
+	fmt.Fscanf(in, "%d", &n)
+	for kase := 1; kase <= n; kase++ {
+		if kase > 1 {
+			fmt.Fscanln(in)
+		}
+		grid = make([][]byte, 3)
+		for i := range grid {
+			fmt.Fscanf(in, "%s", &line)
+			grid[i] = []byte(line)
+		}
+		if solve() {
+			fmt.Fprintln(out, "yes")
+		} else {
+			fmt.Fprintln(out, "no")
+		}
+	}
+}
---
+++ 10363/10363.in
@@ -0,0 +1,8 @@
+2
+X.O
+OO.
+XXX
+
+O.X
+XX.
+OOO
