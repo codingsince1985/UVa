6311c39b4dad93da0d78dd34585eaa115b3e25c7
Jerry Zhao <jchao@y7mail.com>
2017-02-22 00:04:47 +1100

    code clean up

--- 231/231.go
+++ 231/231.go
@@ -8,13 +8,12 @@ import (
 )
 
 func lds(h []int) int {
-	max := 1
 	l := len(h)
 	m := make([]int, l)
 	for i := range m {
 		m[i] = 1
 	}
-
+	max := 1
 	for i := 1; i < l; i++ {
 		for j := 0; j < i; j++ {
 			if h[i] < h[j] && m[i] <= m[j] {
@@ -37,22 +36,16 @@ func main() {
 	var h []int
 	var tmp int
 	count := 0
-	first := true
 	for {
-		if fmt.Fscanf(in, "%d", &tmp); first && tmp == -1 {
+		if fmt.Fscanf(in, "%d", &tmp); tmp == -1 && len(h) == 0 {
 			break
 		}
-		if first {
-			first = false
-			count++
-		}
 		if tmp == -1 {
-			if count > 1 {
+			if count++; count > 1 {
 				fmt.Fprintln(out)
 			}
 			fmt.Fprintf(out, "Test #%d:\n", count)
 			fmt.Fprintf(out, "  maximum possible interceptions: %d\n", lds(h))
-			first = true
 			h = nil
 		} else {
 			h = append(h, tmp)
