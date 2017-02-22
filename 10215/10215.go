f73544764d83a76d5699d65102caa480252c2352
Jerry Zhao <jchao@y7mail.com>
2017-02-23 00:00:22 +1100

    UVa 10215 - The Largest/Smallest Box ...

---
+++ 10215/10215.go
@@ -0,0 +1,31 @@
+// UVa 10215 - The Largest/Smallest Box ...
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+)
+
+func min(a, b float64) float64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	in, _ := os.Open("10215.in")
+	defer in.Close()
+	out, _ := os.Create("10215.out")
+	defer out.Close()
+
+	var l, w float64
+	for {
+		if _, err := fmt.Fscanf(in, "%f%f", &l, &w); err != nil {
+			break
+		}
+		fmt.Fprintf(out, "%.3f %.3f %.3f\n", (l+w-math.Sqrt(l*l-l*w+w*w))/6, 0.0, min(l, w)/2)
+	}
+}
