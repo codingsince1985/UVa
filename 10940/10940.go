463f7290d076c5fbdbbab361035089eec5c3c70b
Jerry Zhao <jchao@y7mail.com>
2017-03-02 07:20:06 +1100

    UVa 10940 - Throwing cards away II

---
+++ 10940/10940.go
@@ -0,0 +1,39 @@
+// UVa 10940 - Throwing cards away II
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const MAX = 500000
+
+var cache = func() []int {
+	cache := make([]int, MAX+1)
+	cache[0], cache[1] = 0, 1
+	for n, i := 1, 2; i <= MAX; i++ {
+		cache[i] = n * 2
+		if n*2 == i {
+			n = 1
+		} else {
+			n++
+		}
+	}
+	return cache
+}()
+
+func main() {
+	in, _ := os.Open("10940.in")
+	defer in.Close()
+	out, _ := os.Create("10940.out")
+	defer out.Close()
+
+	var n int
+	for {
+		if fmt.Fscanf(in, "%d", &n); n == 0 {
+			break
+		}
+		fmt.Fprintln(out, cache[n])
+	}
+}
