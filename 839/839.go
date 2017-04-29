ed515cffb2bb19cfd76737992a093f6dae75e335
Jerry Zhao <jchao@y7mail.com>
2017-04-30 00:00:22 +1000

    UVa 839 - Not so Mobile

---
+++ 839/839.go
@@ -0,0 +1,63 @@
+// UVa 839 - Not so Mobile
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var in *os.File
+
+type mobile struct {
+	wl, dl, wr, dr int
+	left, right    *mobile
+}
+
+func readLine() *mobile {
+	var m mobile
+	fmt.Fscanf(in, "%d%d%d%d", &m.wl, &m.dl, &m.wr, &m.dr)
+	if m.wl == 0 {
+		m.left = readLine()
+	}
+	if m.wr == 0 {
+		m.right = readLine()
+	}
+	return &m
+}
+
+func solve(root *mobile) (bool, int) {
+	var ok bool
+	if root.left != nil {
+		if ok, root.wl = solve(root.left); !ok {
+			return false, -1
+		}
+	}
+	if root.right != nil {
+		if ok, root.wr = solve(root.right); !ok {
+			return false, -1
+		}
+	}
+	return root.wl*root.dl == root.wr*root.dr, root.wl + root.wr
+}
+
+func main() {
+	in, _ = os.Open("839.in")
+	defer in.Close()
+	out, _ := os.Create("839.out")
+	defer out.Close()
+
+	var kase int
+	for fmt.Fscanf(in, "%d", &kase); kase > 0; kase-- {
+		fmt.Fscanln(in)
+		root := readLine()
+		if ok, _ := solve(root); ok {
+			fmt.Fprintln(out, "YES")
+		} else {
+			fmt.Fprintln(out, "NO")
+		}
+		if kase > 1 {
+			fmt.Fprintln(out)
+		}
+	}
+}
