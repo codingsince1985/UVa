c094a294f56e87f7935328aac7b2cde6d50a97ac
Jerry Zhao <jchao@y7mail.com>
2017-05-17 17:33:52 +1000

    UVa 10177 - (2/3/4)-D Sqr/Rects/Cubes/Boxes?

---
+++ 10177/10177.go
@@ -0,0 +1,38 @@
+// UVa 10177 - (2/3/4)-D Sqr/Rects/Cubes/Boxes?
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var out *os.File
+
+func solve(n int) {
+	s2 := n * (n + 1) * (2*n + 1) / 6
+	r2 := (n+1)*n/2*(n+1)*n/2 - s2
+	s3 := n * (n + 1) / 2 * n * (n + 1) / 2
+	r3 := (n+1)*n/2*(n+1)*n/2*(n+1)*n/2 - s3
+	var s4 int
+	for i := 1; i <= n; i++ {
+		s4 += i * i * i * i
+	}
+	r4 := (n+1)*n/2*(n+1)*n/2*(n+1)*n/2*(n+1)*n/2 - s4
+	fmt.Fprintln(out, s2, r2, s3, r3, s4, r4)
+}
+
+func main() {
+	in, _ := os.Open("10177.in")
+	defer in.Close()
+	out, _ = os.Create("10177.out")
+	defer out.Close()
+
+	var n int
+	for {
+		if _, err := fmt.Fscanf(in, "%d", &n); err != nil {
+			break
+		}
+		solve(n)
+	}
+}
