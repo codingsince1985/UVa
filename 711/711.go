7282fc01a52df01a64c46fc6cc9e926add57d196
Jerry Zhao <jchao@y7mail.com>
2017-06-10 00:17:03 +1000

    UVa 711 - Dividing up

---
+++ 711/711.go
@@ -0,0 +1,59 @@
+// UVa 711 - Dividing up
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const max = 20000 * 6
+
+func solve(sum int, values [6]int) bool {
+	if sum%2 != 0 {
+		return false
+	}
+	dp := make([]int, max+1)
+	for i := 0; i <= values[0]; i++ {
+		dp[i] = 1
+	}
+	for i := 2; i <= 6; i++ {
+		for j := sum / 2; j >= 0; j-- {
+			if dp[j] == 0 {
+				continue
+			}
+			for k := 0; k <= values[i-1]; k++ {
+				x := k*i + j
+				if x > sum/2 || dp[x] >= i {
+					break
+				}
+				dp[x] = i
+			}
+		}
+	}
+	return dp[sum/2] != 0
+}
+
+func main() {
+	in, _ := os.Open("711.in")
+	defer in.Close()
+	out, _ := os.Create("711.out")
+	defer out.Close()
+
+	var values [6]int
+	for kase := 1; ; kase++ {
+		var sum int
+		for i := range values {
+			fmt.Fscanf(in, "%d", &values[i])
+			sum += values[i] * (i + 1)
+		}
+		if sum == 0 {
+			break
+		}
+		if fmt.Fprintf(out, "Collection #%d:\n", kase); solve(sum, values) {
+			fmt.Fprintln(out, "Can be divided.\n")
+		} else {
+			fmt.Fprintln(out, "Can't be divided.\n")
+		}
+	}
+}
