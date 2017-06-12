5e7502f1256f68da27156479152ddc238c576b75
Jerry Zhao <jchao@y7mail.com>
2017-06-12 11:55:55 +1000

    UVa 362 - 18,000 Seconds Remaining

---
+++ 362/362.go
@@ -0,0 +1,46 @@
+// UVa 362 - 18,000 Seconds Remaining
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func output(out *os.File, size, b5 int) {
+	if b5 == 0 {
+		fmt.Fprintln(out, "   Time remaining: stalled")
+	} else {
+		remaining := float64(size*5) / float64(b5)
+		if remaining != float64(int(remaining)) {
+			remaining = float64(int(remaining + 1))
+		}
+		fmt.Fprintf(out, "   Time remaining: %.0f seconds\n", remaining)
+	}
+}
+
+func main() {
+	in, _ := os.Open("362.in")
+	defer in.Close()
+	out, _ := os.Create("362.out")
+	defer out.Close()
+
+	var size, b int
+	for kase := 1; ; kase++ {
+		if fmt.Fscanf(in, "%d", &size); size == 0 {
+			break
+		}
+		fmt.Fprintf(out, "Output for data set %d, %d bytes:\n", kase, size)
+		var count int
+		for soFar, b5 := 0, 0; soFar < size; {
+			fmt.Fscanf(in, "%d", &b)
+			b5 += b
+			soFar += b
+			if count++; count%5 == 0 {
+				output(out, size-soFar, b5)
+				b5 = 0
+			}
+		}
+		fmt.Fprintf(out, "Total time: %d seconds\n\n", count)
+	}
+}
