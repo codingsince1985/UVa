ecd137ca941630300f9028455d7892c26262ea01
Jerry Zhao <jchao@y7mail.com>
2017-06-24 00:03:16 +1000

    UVa 11879 - Multiple of 17

---
+++ 11879/11879.go
@@ -0,0 +1,43 @@
+// UVa 11879 - Multiple of 17
+
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"os"
+)
+
+var (
+	five      = big.NewInt(5)
+	ten       = big.NewInt(10)
+	seventeen = big.NewInt(17)
+)
+
+func solve(n string) bool {
+	var num, last big.Int
+	num.SetString(n, 10)
+	num.DivMod(&num, ten, &last)
+	num.Sub(&num, last.Mul(&last, five))
+	num.DivMod(&num, seventeen, &last)
+	return last.Int64() == 0
+}
+
+func main() {
+	in, _ := os.Open("11879.in")
+	defer in.Close()
+	out, _ := os.Create("11879.out")
+	defer out.Close()
+
+	var n string
+	for {
+		if fmt.Fscanf(in, "%s", &n); n == "0" {
+			break
+		}
+		if solve(n) {
+			fmt.Fprintln(out, 1)
+		} else {
+			fmt.Fprintln(out, 0)
+		}
+	}
+}
