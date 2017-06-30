6628ad06f2f64156c3abc04537bd08e2172601af
Jerry Zhao <jchao@y7mail.com>
2017-07-01 00:03:45 +1000

    UVa 796 - Critical Links

---
+++ 796/796.go
@@ -0,0 +1,105 @@
+// UVa 796 - Critical Links
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+)
+
+var (
+	out         *os.File
+	matrix, cut [][]bool
+	visited     []bool
+	depth, low  []int
+)
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func dfs(current, d, parent int) int {
+	m := math.MaxInt32
+	depth[current] = d
+	for i := range matrix {
+		if matrix[i][current] {
+			if !visited[i] {
+				visited[i] = true
+				l := dfs(i, d+1, current)
+				if l > d {
+					cut[i][current], cut[current][i] = true, true
+				}
+				m = min(m, l)
+			} else {
+				if i != parent {
+					m = min(m, depth[i])
+				}
+			}
+		}
+	}
+	low[current] = m
+	return low[current]
+}
+
+func solve(n int) {
+	low = make([]int, n)
+	depth = make([]int, n)
+	cut = make([][]bool, n)
+	for i := range cut {
+		cut[i] = make([]bool, n)
+	}
+	visited = make([]bool, n)
+	for i := range matrix {
+		if !visited[i] {
+			visited[i] = true
+			dfs(i, 1, -1)
+		}
+	}
+	var count int
+	var output string
+	for i := 0; i < n-1; i++ {
+		for j := i + 1; j < n; j++ {
+			if cut[i][j] {
+				count++
+				output += fmt.Sprintf("%d - %d\n", i, j)
+			}
+		}
+	}
+	fmt.Fprintf(out, "%d critical links\n%s\n", count, output)
+}
+
+func main() {
+	in, _ := os.Open("796.in")
+	defer in.Close()
+	out, _ = os.Create("796.out")
+	defer out.Close()
+
+	var n, m, s, t int
+	first := true
+	for {
+		if first {
+			first = false
+		} else {
+			fmt.Fscanln(in)
+		}
+		if _, err := fmt.Fscanf(in, "%d", &n); err != nil {
+			break
+		}
+		matrix = make([][]bool, n)
+		for i := range matrix {
+			matrix[i] = make([]bool, n)
+		}
+		for i := 0; i < n; i++ {
+			for fmt.Fscanf(in, "%d (%d)", &s, &m); m > 0; m-- {
+				fmt.Fscanf(in, "%d", &t)
+				matrix[s][t], matrix[t][s] = true, true
+			}
+		}
+		fmt.Fscanln(in)
+		solve(n)
+	}
+}
