a5f3ab3898b778fb6934059a5e198462ba63e692
Jerry Zhao <jchao@y7mail.com>
2017-08-24 07:35:20 +1000

    UVa 10306 - e-Coins

---
+++ 10306/10306.go
@@ -0,0 +1,66 @@
+// UVa 10306 - e-Coins
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+)
+
+type ecoin struct{ conventional, infoTechnological int }
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func solve(s int, ecoins []ecoin) int {
+	dp := make([][]int, s+1)
+	for i := range dp {
+		dp[i] = make([]int, s+1)
+		for j := range dp[i] {
+			dp[i][j] = math.MaxInt32
+		}
+	}
+	dp[0][0] = 0
+	for _, coin := range ecoins {
+		for i := coin.conventional; i <= s; i++ {
+			for j := coin.infoTechnological; j <= s; j++ {
+				dp[i][j] = min(dp[i][j], dp[i-coin.conventional][j-coin.infoTechnological]+1)
+			}
+		}
+	}
+	coins := math.MaxInt32
+	for i := 0; i <= s; i++ {
+		for j := 0; j <= s; j++ {
+			if i*i+j*j == s*s {
+				coins = min(coins, dp[i][j])
+			}
+		}
+	}
+	return coins
+}
+
+func main() {
+	in, _ := os.Open("10306.in")
+	defer in.Close()
+	out, _ := os.Create("10306.out")
+	defer out.Close()
+
+	var n, m, s int
+	for fmt.Fscanf(in, "%d", &n); n > 0; n-- {
+		fmt.Fscanf(in, "%d%d", &m, &s)
+		ecoins := make([]ecoin, m)
+		for i := range ecoins {
+			fmt.Fscanf(in, "%d%d", &ecoins[i].conventional, &ecoins[i].infoTechnological)
+		}
+		if coins := solve(s, ecoins); coins == math.MaxInt32 {
+			fmt.Fprintln(out, "not possible")
+		} else {
+			fmt.Fprintln(out, coins)
+		}
+	}
+}
