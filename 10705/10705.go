b06a1eb8eb9594bfa7096fd110bff0f7e4521995
Jerry Zhao <jchao@y7mail.com>
2017-10-08 10:41:16 +1100

    UVa 10705 - The Fun Number System

---
+++ 10705/10705.go
@@ -0,0 +1,41 @@
+// UVa 10705 - The Fun Number System
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func solve(n int64, np string) string {
+	var funk string
+	for i := len(np) - 1; i >= 0; i-- {
+		lastBit := n & 1
+		funk = fmt.Sprint(lastBit) + funk
+		n /= 2
+		switch {
+		case lastBit == 1 && np[i] == 'n' && n > 0:
+			n += 1
+		case lastBit == 1 && np[i] == 'p' && n < 0:
+			n -= 1
+		}
+	}
+	if n != 0 {
+		return "Impossible"
+	}
+	return funk
+}
+
+func main() {
+	in, _ := os.Open("10705.in")
+	defer in.Close()
+	out, _ := os.Create("10705.out")
+	defer out.Close()
+
+	var kase, k, n int64
+	var np string
+	for fmt.Fscanf(in, "%d", &kase); kase > 0; kase-- {
+		fmt.Fscanf(in, "%d\n%s\n%d", &k, &np, &n)
+		fmt.Fprintln(out, solve(n, np))
+	}
+}
