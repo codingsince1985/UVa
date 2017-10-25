fb7cc6babdca1a3eaf4bb288b3883beba1e6fec0
Jerry Zhao <jchao@y7mail.com>
2017-10-25 12:41:32 +1100

    code clean up

--- 133/133.go
+++ 133/133.go
@@ -8,6 +8,66 @@ import (
 	"strings"
 )
 
+func solve(n, k, m int, out *os.File) {
+	dole := make([]int, n)
+	for i := range dole {
+		dole[i] = i + 1
+	}
+	pk, pm := 0, n-1
+	outNum := 0
+	var outs []string
+	for {
+		for i := 1; i < k; i++ {
+			for {
+				if pk++; pk == n {
+					pk = 0
+				}
+				if dole[pk] != -1 {
+					break
+				}
+			}
+		}
+		for i := 1; i < m; i++ {
+			for {
+				if pm--; pm == -1 {
+					pm = n - 1
+				}
+				if dole[pm] != -1 {
+					break
+				}
+			}
+		}
+		if pk == pm {
+			outNum++
+			outs = append(outs, fmt.Sprintf("% 3d", dole[pk]))
+		} else {
+			outNum += 2
+			outs = append(outs, fmt.Sprintf("% 3d% 3d", dole[pk], dole[pm]))
+		}
+		if outNum == len(dole) {
+			break
+		}
+		dole[pk], dole[pm] = -1, -1
+		for {
+			if pk++; pk == n {
+				pk = 0
+			}
+			if dole[pk] != -1 {
+				break
+			}
+		}
+		for {
+			if pm--; pm == -1 {
+				pm = n - 1
+			}
+			if dole[pm] != -1 {
+				break
+			}
+		}
+	}
+	fmt.Fprintln(out, strings.Join(outs, ","))
+}
+
 func main() {
 	in, _ := os.Open("133.in")
 	defer in.Close()
@@ -19,62 +79,6 @@ func main() {
 		if fmt.Fscanf(in, "%d%d%d", &n, &k, &m); n == 0 {
 			break
 		}
-		dole := make([]int, n)
-		for i := range dole {
-			dole[i] = i + 1
-		}
-		pk, pm := 0, n-1
-		outNum := 0
-		var outs []string
-		for {
-			for i := 1; i < k; i++ {
-				for {
-					if pk++; pk == n {
-						pk = 0
-					}
-					if dole[pk] != -1 {
-						break
-					}
-				}
-			}
-			for i := 1; i < m; i++ {
-				for {
-					if pm--; pm == -1 {
-						pm = n - 1
-					}
-					if dole[pm] != -1 {
-						break
-					}
-				}
-			}
-			if pk == pm {
-				outNum++
-				outs = append(outs, fmt.Sprintf("% 3d", dole[pk]))
-			} else {
-				outNum += 2
-				outs = append(outs, fmt.Sprintf("% 3d% 3d", dole[pk], dole[pm]))
-			}
-			if outNum == len(dole) {
-				break
-			}
-			dole[pk], dole[pm] = -1, -1
-			for {
-				if pk++; pk == n {
-					pk = 0
-				}
-				if dole[pk] != -1 {
-					break
-				}
-			}
-			for {
-				if pm--; pm == -1 {
-					pm = n - 1
-				}
-				if dole[pm] != -1 {
-					break
-				}
-			}
-		}
-		fmt.Fprintln(out, strings.Join(outs, ","))
+		solve(n, k, m, out)
 	}
 }
