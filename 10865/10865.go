99965d1d1c5dc7022fab1bdd205f69596966e293
Jerry Zhao <jchao@y7mail.com>
2017-11-19 10:26:07 +1100

    UVa 10865 - Brownie Points

---
+++ 10865/10865.go
@@ -0,0 +1,45 @@
+// UVa 10865 - Brownie Points
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type brownie struct{ x, y int }
+
+func solve(n int, brownies []brownie) (int, int) {
+	var stan, ollie int
+	center := brownies[(n-1)/2]
+	for _, b := range brownies {
+		pos := (b.x - center.x) * (b.y - center.y)
+		switch {
+		case pos > 0:
+			stan++
+		case pos < 0:
+			ollie++
+		}
+	}
+	return stan, ollie
+}
+
+func main() {
+	in, _ := os.Open("10865.in")
+	defer in.Close()
+	out, _ := os.Create("10865.out")
+	defer out.Close()
+
+	var n int
+	for {
+		if fmt.Fscanf(in, "%d", &n); n == 0 {
+			break
+		}
+		brownies := make([]brownie, n)
+		for i := range brownies {
+			fmt.Fscanf(in, "%d%d", &brownies[i].x, &brownies[i].y)
+		}
+		stan, ollie := solve(n, brownies)
+		fmt.Fprintln(out, stan, ollie)
+	}
+}
